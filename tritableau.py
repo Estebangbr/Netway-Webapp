a3b78783bcc573240e9629d2caba41c04cd4c075
Asvinthan <asvinthan29@hotmail.fr>
2021-07-26 21:47:41 +0200

    Test du script fait

---
+++ tritableau.py
@@ -0,0 +1,20 @@
+def tri_selection(tab):
+   for i in range(len(tab)):
+      # Trouver le min
+       min = i
+       for j in range(i+1, len(tab)):
+           if tab[min] > tab[j]:
+               min = j
+                
+       tmp = tab[i]
+       tab[i] = tab[min]
+       tab[min] = tmp
+   return tab
+# Programme principale pour tester le code ci-dessus
+tab = [98, 22, 15, 32, 2, 79, 65,34]
+ 
+tri_selection(tab)
+ 
+print ("Le tableau trié est:")
+for i in range(len(tab)):
+    print ("%d" %tab[i])
