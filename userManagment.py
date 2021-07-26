b3601903af0cb34a3e18518254a156c5a3505ff7
yohann <yohann.vql@gmail.com>
2021-07-26 17:32:58 +0200

    Ajout du fichier userManagment + création d'utilisateur dans l'active directory

---
+++ userManagment.py
@@ -0,0 +1,36 @@
+  # Import des modules pour l'execution du script
+    import ldap
+    import ldap.modlist as modlist
+
+
+    adduser = raw_input('entrez le nouvel utilisateur\n' ')
+
+    # ouvre la connexion ldap du serveur windows 192.168.1.2
+    print('initializing ..')
+
+    conn = ldap.initialize('ldap://192.168.1.2')
+    conn.protocol_version = 3
+    conn.set_option(ldap.OPT_REFERRALS, 0)
+    conn.simple_bind_s('Administrateur@paris.local', 'Paris16..')
+
+    # Dn du nouvel utilisateur
+    DN = ('CN=' + print('adduser') + ',OU=utilisateurs,DC=paris,DC=local')
+
+    # attribue du nouvel utilisateur
+    modlist = {
+    'objectClass': ['top', 'person', 'organizationalPerson', 'user'],
+    'cn': print('adduser'),
+    'givenName': print('adduser'),
+    'displayName': print('adduser'),
+    'sAMAccountName': print('adduser'),
+    'userAccountControl': '514',
+    'userPrincipalName': (print('adduser') + '@paris.local'),
+    'mail': (print('adduser') + '@paris.local'),
+    'userPassword': '@Password16..',
+    'description': 'test'
+    }
+
+    # Creation du nouvel utilisateur
+    result = conn.add_s(DN, ldap.modlist.addModlist(modlist))
+    print('Utilisateur créé')
+    exit
